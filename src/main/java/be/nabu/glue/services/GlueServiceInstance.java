9ef1741c1ad65a91fd63a00cebd97c1df1ea9e6d
alex <alex@nabu.be>
2016-11-06 08:08:08 +0100

    Added a post processor to resolve lazy lists, otherwise the glue context may be gone which can make it impossible to resolve them later

--- src/main/java/be/nabu/glue/services/GlueServiceInstance.java
+++ src/main/java/be/nabu/glue/services/GlueServiceInstance.java
@@ -1,8 +1,13 @@
 package be.nabu.glue.services;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import be.nabu.glue.api.PostProcessor;
+import be.nabu.glue.core.impl.methods.v2.SeriesMethods;
 import be.nabu.glue.utils.ScriptRuntime;
 import be.nabu.libs.services.api.ExecutionContext;
 import be.nabu.libs.services.api.Service;
@@ -30,6 +35,21 @@ public class GlueServiceInstance implements ServiceInstance {
 			}
 		}
 		ScriptRuntime runtime = new ScriptRuntime(service.getScript(), new CombinedExecutionContextImpl(executionContext, service.getEnvironment(), service.getLabelEvaluator()), map);
+		
+		// add a post processor to automatically resolve iterables
+		// the returned variables could be used outside of a glue context which makes lazy resolving sometimes impossible (depending on the type of series)
+		List<PostProcessor> postProcessors = new ArrayList<PostProcessor>();
+		postProcessors.add(new PostProcessor() {
+			@Override
+			public void postProcess(be.nabu.glue.api.ExecutionContext context) {
+				for (String key : context.getPipeline().keySet()) {
+					if (context.getPipeline().get(key) instanceof Iterable && !(context.getPipeline().get(key) instanceof Collection)) {
+						context.getPipeline().put(key, SeriesMethods.resolve((Iterable<?>) context.getPipeline().get(key)));
+					}	
+				}
+			}
+		});
+		runtime.setPostProcessors(postProcessors);
 		runtime.run();
 		if (runtime.getException() != null) {
 			throw new ServiceException(runtime.getException());
